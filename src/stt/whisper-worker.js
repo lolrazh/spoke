00a3ff413a1b233be4b08f95437a26b629751aae
Sandheep Rajkumar <rajkumar.sandheep@gmail.com>
2025-04-24 20:05:41 +0530

    switched to hf transformers

--- src/stt/whisper-worker.js
+++ src/stt/whisper-worker.js
@@ -1,8 +1,6 @@
-// a good bulk of this is from https://github.com/xenova/whisper-web/blob/main/src/worker.js
-import { pipeline, env } from '@xenova/transformers';
+import { pipeline, env } from '@huggingface/transformers';
 
 // Configuration
-// const MODEL_NAME = 'Xenova/whisper-tiny'; // Old model
 const MODEL_NAME = 'onnx-community/whisper-base'; // Use tiny English-only (pre-quantized) for speed
 const TASK = 'automatic-speech-recognition';
 const LANGUAGE = 'english'; // Model is English-only now
